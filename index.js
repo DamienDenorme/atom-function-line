0e6d06d139da186c939a2bd9705ccefbdea38e5a
Damien Denorme <nanoseconde@gmail.com>
2018-11-11 13:46:26 +0100

    Add configuration of the line color

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 "use babel";
 import FunctionStyler from "./lib/function-styler";
 
-export default new FunctionStyler({
-  // config: atom.config,
-  workspace: atom.workspace
-});
+export default new FunctionStyler();
